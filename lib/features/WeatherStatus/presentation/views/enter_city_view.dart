7b0c551bfe7745b36b3456558c0e06927b38d354
Khaled Khalifa <96270919+khaledkhalifa74@users.noreply.github.com>
2025-03-21 16:09:55 +0200

    add packages and initialize main.dart

---
+++ lib/features/WeatherStatus/presentation/views/enter_city_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_status_app/features/WeatherStatus/presentation/views/widgets/enter_city_view_body.dart';
+
+class EnterCityView extends StatelessWidget {
+  const EnterCityView({super.key});
+  static String id = "EnterCityView";
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: true,
+        backgroundColor: kBGColor,
+        body: EnterCityViewBody(),
+      ),
+    );
+  }
+}
---
+++ lib/features/WeatherStatus/presentation/views/widgets/enter_city_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnterCityViewBody extends StatefulWidget {
+  const EnterCityViewBody({super.key});
+
+  @override
+  State<EnterCityViewBody> createState() => _EnterCityViewBodyState();
+}
+
+class _EnterCityViewBodyState extends State<EnterCityViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
